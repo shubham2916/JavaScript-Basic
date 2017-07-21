ccc9b756bdf4c3994ac390ef8382c6ddf3923286
Shubham Gupta <shubham2916@outlook.com>
2017-07-21 18:49:03 +1000

    added date time file

---
+++ datetime.js
@@ -0,0 +1,17 @@
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+'  '+time;
+console.log(date);
+console.log(dateTime);
+console.log(time);
+
+//var a,b,c;
+//function area  ()
+//{  
+
+
+    
+//}
+//return 
